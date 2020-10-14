167b35d1c4c3339b09c694ba767eec232a6b29a3
eduar <eduar.curi73@outlook.com>
2020-10-14 14:07:06 -0500

    implementando menu de operaciones para el usuario e integrando esas opciones a los condicionales arreglando la validacion de ingreso de datos a siempre mayusculas

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-#SOFTWARE DE PRUEBA
 #CREACION DE UN ACALCULADORA SIMPLE CON FUNCIONES
 
 #DEFINIENDO FUNCIONES
@@ -26,18 +25,23 @@ def division():
     division = number_1 / number_2
     print(division)
 
+print(':::::BIENVENIDO A LA CALCULADORA:::::')
+print('\n> ADICION ("A")') #MEUN DE OPCIONES PARA EL USUARIO
+print('> SUSTRACCION ("S")')
+print('> MULTIPLICACION ("M")')
+print('> DIVISION ("D")')
 #PIDIENDO AL USUARIO LA INFORMACION DE LA OPERACION MATEMATICA
-operation = input('Ingrese la inicial de la operacion que desea realizar: ')
+operation = input('\nINGRESE LA OPCIÓN QUE DESEA REALIZAR: ').upper()
 #VALIDANDO LA OPCION INTRODUCIDA POR EL USUARIO
-while operation != 's' and operation != 'r' and operation != 'm' and operation != 'd':
+while operation != 'A' and operation != 'S' and operation != 'M' and operation != 'D':
     print('ERROR, operacion no encontrada')
-    operation = input('Ingrese la inicial del aoperacion que desea realizar nuevamente: ')
+    operation = input('Ingrese la inicial del aoperacion que desea realizar nuevamente: ').upper()
 #DESARROLLO DE ACUERDO A PETICION
-if operation == 's':
+if operation == 'A':
     addition()
-elif operation == 'r':
+elif operation == 'S':
     subtraction()
-elif operation == 'm':
+elif operation == 'M':
     multiplication()
 else:
     division()
